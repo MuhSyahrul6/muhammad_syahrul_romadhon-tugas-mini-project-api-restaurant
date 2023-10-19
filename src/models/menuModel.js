754d1089da27f22e7d535f5e29e9d7b42db252e1
Muhammad Syahrul Romadhon <135720528+MuhSyahrul6@users.noreply.github.com>
2023-10-19 19:17:17 +0700

    Update menuModel.js

--- src/models/menuModel.js
+++ src/models/menuModel.js
@@ -1,24 +1,59 @@
 const db = require("../../db/config")
 
 const menuModel = {}
-menuModel.getAll = () => {
-    return db.all("SELECT * FROM menu",(err,rows) => {
-        if(err) {
-            throw err
-        }else {
-            return rows
+menuModel.getAll = (callback) => {
+    var rowData
+    const query = db.all("SELECT * FROM menu", (err, rows) => {
+        if (err) {
+            callback(err, null);
+        } else {
+            callback(null, rows);
         }
-    })
-}
+    });
+    console.log(rowData);
+    return query
+};
+
 // lanjutkan disini
 menuModel.create = (data) => {
-    return db.run(`INSERT INTO menu (name,price) VALUES ('${data.name,data.price}')`,(err,rows) => {
-        if(err) {
-            throw err
-        }else {
-            return rows
+    return db.run(`INSERT INTO menu (item, price) VALUES ('${data.item}', '${data.price}' )`, (err, rows) => {
+        if (err) {
+            throw err;
+        } else {
+            return rows;
         }
-    })
+    });
 }
+
+menuModel.findById = (id, callback) => {
+    return db.get(`SELECT * FROM menu WHERE id = ${id}`, (err, rows) => {
+        if (err) {
+            callback(err, null);
+        } else {
+            callback(null, rows);
+        }
+    });
+}
+
+
+menuModel.update = (id, data, callback) => {
+    return db.run(`UPDATE menu SET item = '${data.item}', price = '${data.price}' WHERE id = ${id}`, (err, rows) => {
+        if (err) {
+            callback(err, null);
+        } else {
+            callback(null, rows);
+        }
+    });
+}
+
+menuModel.delete = (id, callback) => {
+    return db.run(`DELETE FROM menu WHERE id = ${id}`, (err, rows) => {
+        if (err) {
+            callback(err, null);
+        } else {
+            callback(null, rows);
+        }
+    });
+};
 
 module.exports = menuModel
