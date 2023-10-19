b3948345b1a992a597ee568dbee5dc9c8235cfa8
Muhammad Syahrul Romadhon <135720528+MuhSyahrul6@users.noreply.github.com>
2023-10-19 19:08:59 +0700

    Update config.js

--- db/config.js
+++ db/config.js
@@ -1,16 +1,42 @@
-const sqlite = require("sqlite3")
+const sqlite = require("sqlite3");
 
 const db = new sqlite.Database("restaurant.db");
 
 db.serialize(() => {
-    db.run(`CREATE TABLE IF NOT EXISTS menu (id INTEGER PRIMARY KEY AUTOINCREMENT, item TEXT, price REAL,created_at DATETIME default CURRENT_TIMESTAMP,
-        updated_at DATETIME default CURRENT_TIMESTAMP);`)
-    db.run(`CREATE TABLE IF NOT EXISTS customer (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, address TEXT,email TEXT,created_at DATETIME default CURRENT_TIMESTAMP,
-        updated_at DATETIME default CURRENT_TIMESTAMP);`)
-    db.run(`CREATE TABLE IF NOT EXISTS categories (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT,created_at DATETIME default CURRENT_TIMESTAMP,
-        updated_at DATETIME default CURRENT_TIMESTAMP);`)
-})
-db.on('open',() => console.log("Success generate table !"))
-db.on('error',() => console.log("database error!"))
+    db.run(`PRAGMA foreign_keys = ON;`);
+    db.run(`CREATE TABLE IF NOT EXISTS menu (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        item TEXT, 
+        price REAL,
+        created_at DATETIME default CURRENT_TIMESTAMP,
+        updated_at DATETIME default CURRENT_TIMESTAMP);`);
+
+    db.run(`CREATE TABLE IF NOT EXISTS customer (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        name TEXT, 
+        address TEXT,
+        email TEXT,
+        created_at DATETIME default CURRENT_TIMESTAMP,
+        updated_at DATETIME default CURRENT_TIMESTAMP);`);
+
+    db.run(`CREATE TABLE IF NOT EXISTS categories (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        name TEXT,
+        created_at DATETIME default CURRENT_TIMESTAMP,
+        updated_at DATETIME default CURRENT_TIMESTAMP);`);
+
+    db.run(`CREATE TABLE IF NOT EXISTS orders (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            customer_id INTEGER NOT NULL,
+            menu_id INTEGER NOT NULL,
+            qty INTEGER NOT NULL,
+            order_date DATE NOT NULL, 
+            created_at DATETIME default CURRENT_TIMESTAMP,
+            updated_at DATETIME default CURRENT_TIMESTAMP,
+            FOREIGN KEY (customer_id) REFERENCES customer(id),
+            FOREIGN KEY (menu_id) REFERENCES menu(id));`);
+});
+db.on('open', () => console.log("Success generate table !"));
+db.on('error', () => console.log("database error!"));
 
 module.exports = db
